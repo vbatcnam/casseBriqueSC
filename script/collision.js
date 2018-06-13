28eb94817c7eaa5b70bad393244e9ab248804ff7
vbatcnam <40175721+vbatcnam@users.noreply.github.com>
2018-06-13 15:31:56 +0200

    Add files via upload
    quelques corrections

--- script/collision.js
+++ script/collision.js
@@ -1,3 +1,30 @@
-gereCollision(objet1, objet2){
-	
+function gereCollision(objet1, objet2){
+	return;
 }
+
+// function gereCollision() {
+	// for(var c=0; c<this.brickColumnCount; c++) {
+		// for(var r=0; r<this.brickRowCount; r++) {
+			// let briqueEnCours = this.tabBriques[c][r];
+			// if(briqueEnCours.status == 1){
+				// //si la brique est touchée
+				// if(this.balle.x > briqueEnCours.x && this.balle.x < briqueEnCours.x + briqueEnCours.width && this.balle.y > briqueEnCours.y && this.balle.y < briqueEnCours.y + briqueEnCours.height){
+						// this.balle.dy = -this.balle.dy;
+						// if(briqueEnCours.force == 1){
+							// briqueEnCours.force = 0;
+							// briqueEnCours.color = briqueEnCours.colorise();
+							// briqueEnCours.draw();
+						// }else{
+							// briqueEnCours.status = 0;
+							// this.score++;
+						// }
+						// if(this.score == this.brickRowCount*this.brickColumnCount) {
+							// alert("Bravo !");
+							// return;
+						// }
+				// }
+			// }
+		// }
+	// }
+// }
+
