f56903716713982242090bd717f22264d5801d2f
Vero <vero@famille.fr>
2018-10-31 19:09:14 +0100

    modif constructeur et ajout fonction parseInstr

--- script/syntaxeSimplifieeSC.js
+++ script/syntaxeSimplifieeSC.js
@@ -13,10 +13,23 @@ function SCEVT(ps_nom) {
 	return g_AllSCevents[ps_nom]
 }
 
+function parseInstr(ps_texte, pArrayS_nomInstr){
+	// ps_methExtractionReste = 'reste' | 'nombre'
+	for(let ls_nomInstr of pArrayS_nomInstr){
+		if(ps_texte.startsWith(ls_nomInstr)){
+			const ls_reste = ps_texte.substring(ls_nomInstr.length)
+			const lArray_nombre = ps_texte.match(/\d+/g)
+			return {instr: ls_nomInstr, reste: ls_reste, nombres: lArray_nombre}
+		}
+	}
+	return false
+}
+
 //permet de créer un cube en même temps que l'objet.
 class SCCube extends SC.cube().constructor {
-	constructor() {
+	constructor(...pArray_args) {
 		super(null, null)
+		if(! pArray_args.length) pArray_args = [{}]
 		this.o = this
 		this.evtKillInstance = SC.evt('kill instance')
 		
@@ -25,14 +38,58 @@ class SCCube extends SC.cube().constructor {
 		const lArray_prog = []
 		for(let ls_nomMeth of lArray_methodes) {
 			if(ls_nomMeth.substring(0,1) == '$') {
-				if(ls_nomMeth.substring(0,15) == '$actionForever_') {
-					// lArray_prog.push(SC.repeat( SC.forever, ...this[ls_nomMeth]() ))
+				const {instr, reste, nombres} = parseInstr(ls_nomMeth, [
+					'$actionForever_', '$repeat', '$on_', '$_',
+					'$const_', '$publicConst_',
+					'$var_', '$publicVar_',
+					'$property_', '$publicProperty_'
+				])
+				if(instr == '$actionForever_') {
 					lArray_prog.push( SC.action(this[ls_nomMeth].bind(this), SC.forever) )
-					// console.log(ls_nomMeth)
-				}else if(ls_nomMeth.substring(0,7) == '$repeat'){
-					const ln_nbFois = parseInt(ls_nomMeth.match(/\d+/g)[0])
+				}else if(instr == '$repeat'){
+					const ln_nbFois = parseInt(nombres[0])
 					lArray_prog.push(SC.repeat( ln_nbFois, ...this[ls_nomMeth]() ))
-				}else if(ls_nomMeth.substring(1,2) == '_') {
+				}else if(instr == '$const_' || instr == '$publicConst_' || instr == '$var_' || instr == '$publicVar_'){
+					const ls_nomVar = reste
+					const lArray_args = (pArray_args[0][ls_nomMeth] == undefined)
+							? []
+							: pArray_args[0][ls_nomMeth]
+					if(instr == '$const_' || instr == '$publicConst_'){
+						this[ls_nomVar] = this[ls_nomMeth](...lArray_args)
+					}else{
+						this[ls_nomVar] = this[ls_nomMeth].bind(this, ...lArray_args)
+					}
+					if(instr == '$publicVar_' || instr == '$publicConst_'){
+						lArray_prog.push(SC.generate(
+							SCEVT(ls_nomVar),
+							this[ls_nomVar],
+							SC.forever
+						))
+					}
+				}else if(instr == '$property_' || instr == '$publicProperty_'){
+					const ls_nomProperty = reste
+					const lArray_parts = this[ls_nomMeth]()
+					this[ls_nomProperty] = this.defineProperty(...lArray_parts)
+					lArray_prog.push(SC.repeat( SC.forever, this[ls_nomProperty] ))
+					if(instr == '$publicProperty_'){
+						lArray_prog.push(SC.generate(
+							SCEVT(ls_nomProperty),
+							this[ls_nomProperty].valeur,
+							SC.forever
+						))
+					}
+				}else if(instr == '$on_'){
+					const ls_nomEvt = ls_nomMeth.match(/_[A-Za-z0-9]+(?=_)/g)[0].substring(1)
+					lArray_prog.push(SC.actionOn(
+						SCEVT(ls_nomEvt),
+						(pArray_allEvt)=>{
+							const lArray_evt = pArray_allEvt[SCEVT(ls_nomEvt)]
+							this[ls_nomMeth](lArray_evt)
+						},
+						undefined,
+						SC.forever
+					))
+				}else if(instr == '$_') {
 					lArray_prog.push( this[ls_nomMeth]() )
 				}
 			}
